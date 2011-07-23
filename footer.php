6776cae87ef7c4ee3720717306499cdfbef40955
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-22 19:15:43 -0700

    cleaned files

--- footer.php
+++ footer.php
@@ -11,3 +11,6 @@
 </div>
 
 <?php wp_footer(); ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php get_header(); ?>
 
-<?php if(have_posts()) : ?>
-	<?php while(have_posts()) : the_post(); ?>
-		<div class="post">
-			<?php the_title(); ?>
-			<?php the_content(); ?>
-
-			<?php echo wpsc_shopping_cart(); ?>
-		</div>
-	<?php endwhile; ?>
-<?php endif; ?>
+<?php while(have_posts()) : the_post(); ?>
+	<?php the_content(); ?>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
-
-</body>
-</html>
