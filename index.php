1e1387bc99b607bd6800cf82203f363348cc6cbf
Quentin Pleplé <quentin.pleple@gmail.com>
2011-07-26 05:51:07 -0700

    fixing float problem

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 <div id="page-content">
     <?php while(have_posts()) : the_post(); ?>
-        <div clas="post">
+        <div class="post">
     		<h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
     		<div class="post-metadata"><?php the_author() ?>, <?php the_time('F jS, Y') ?></div>
     		<?php the_content(); ?>
-        	<div style="clear:both"></div>
         </div>
     <?php endwhile; ?>
     <div style="clear:both"></div>
